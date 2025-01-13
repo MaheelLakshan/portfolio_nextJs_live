0d9f6aa61553460c4f1e4c4329bca2fde8415992
MaheelLakshan <MaheelLakshanra@gmail.com>
2025-01-13 14:55:53 +0530

    Layout was developed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+import './globals.css';
+import { ThemeProvider } from './provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: 'Welcome to Portfolio',
+  description: 'My latest projects can be accessed for you',
 };
 
 export default function RootLayout({
@@ -19,15 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/title-logo.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
