d43761f37d23b5f24408b7e7f7dbafba8c236247
MaheelLakshan <MaheelLakshanra@gmail.com>
2025-01-13 21:39:26 +0530

    recent projects was developed

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -394,7 +394,7 @@ const GridGlobe = () => {
   ];
 
   return (
-    <div className="flex items-center justify-center absolute md:top-12 w-full h-full">
+    <div className="flex items-center justify-center absolute md:top-12 w-full h-full ">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-96 px-4">
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
 
